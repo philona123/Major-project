704c48915a32205fc52e2cda6440e8a700eef915
philona123 <philonasebastian44@gmail.com>
2022-02-07 21:59:25 +0530

    Modified app_page and integrated recommendation_page

--- app_page.py
+++ app_page.py
@@ -5,6 +5,7 @@ from searchjobs_page import show_searchjobs_page
 from analysis_page import show_analysis_page
 from recommendation_page import show_recommendation_page
 
+
 page = st.sidebar.radio("MENU", ["Home", "Search Jobs", "Job Recommendation", "Analysis Result"])
 
 if page == "Home":
@@ -15,4 +16,6 @@ if page == "Job Recommendation":
     show_recommendation_page()
 if page == "Analysis Result":
     show_analysis_page()
+
+
     
--- recommendation_page.py
+++ recommendation_page.py
@@ -1,10 +1,254 @@
 import streamlit as st
+import streamlit.components.v1 as stc 
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use("TkAgg")
+import seaborn as sns
+import pandas as pd 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
+import numpy as np
+
+
+data = pd.read_csv('naukri.csv')
+
+
+data['joblocation_adress'] = data['joblocation_address'].str.split(',')
+pd.set_option('max_rows', 300000)
+
+
+data = data.explode('joblocation_adress')
+pd.set_option('max_rows', 30000)
+
+
+counts = data['joblocation_adress'].value_counts()
+data = data.loc[data['joblocation_adress'].isin(counts.index[counts > 25])]
+
+# Data Cleaning starts
+data['joblocation_adress']=data['joblocation_adress'].replace(('(Bengaluru/Bangalore)',' Bangalore',
+                                                                ' Bangalore ','Bangalore ','Bengaluru/Bangalore ',
+                                                                 ' Bengaluru/Bangalore ',' Bengaluru/Bangalore',
+                                                                'Bengaluru/Bangalore','Bengaluru','NCR Bangalore',
+                                                                'NCR Bangalore ','Near Bangalore', ' Bengaluru/Bangalore',
+                                                                 ' Bengaluru/Bangalore ', ' Bengaluru',
+                                                                 'Bangalore , Bangalore / Bangalore',
+                                                                 'Bangalore , karnataka',' Bengaluru / Bangalore', 
+                                                                 ' Bengaluru / Bangalore ', 'Bengaluru Bangalore',
+                                                                 'India-Karnataka-Bangalore',' bangalore',' karnataka',
+                                                                ' Bengaluru Bangalore'),
+                                                                ('Bangalore','Bangalore','Bangalore','Bangalore','Bangalore',
+                                                                'Bangalore','Bangalore','Bangalore','Bangalore','Bangalore',
+                                                                'Bangalore','Bangalore','Bangalore','Bangalore','Bangalore',
+                                                                 'Bangalore','Bangalore', 'Bangalore','Bangalore','Bangalore',
+                                                                'Bangalore','Bangalore','Bangalore','Bangalore',))
+                                                                
+data['joblocation_adress']=data['joblocation_adress'].replace(('Hyderabad / Secunderabad',' Hyderabad / Secunderabad',
+                                                                 ' Hyderabad / Secunderabad ','Hyderabad / Secunderabad ',
+                                                                ' Hyderabad','Hyderabad ',' Hyderabad ',
+                                                                 'Hyderabad/Secunderabad','Hyderabad/Secunderabad ',
+                                                                ' Hyderabad/Secunderabad ',' Hyderabad/Secunderabad',),
+                                                                ('Hyderabad', 'Hyderabad','Hyderabad','Hyderabad',
+                                                                 'Hyderabad', 'Hyderabad','Hyderabad','Hyderabad',
+                                                                'Hyderabad','Hyderabad','Hyderabad'))
+
+
+  
+data['joblocation_adress']=data['joblocation_adress'].replace(('NAVI MUMBAI',' NAVI MUMBAI','NAVI MUMBAI ',
+                                                                 ' NAVI MUMBAI',' NAVI MUMBAI ','Mumbai , Mumbai',
+                                                                 ' Mumbai',' Mumbai ','Mumbai ','mumbai','Navi Mumbai',
+                                                                ' Navi Mumbai',' Navi Mumbai ','Navi Mumbai ', 
+                                                                 ' Mumbai Suburbs','Mumbai Suburbs ','Mumbai Suburbs',
+                                                                ' Mumbai Suburbs ','mumbai',' mumbai','mumbai ',
+                                                                 ' maharashtra'),
+                                                                ('Mumbai','Mumbai','Mumbai','Mumbai','Mumbai','Mumbai',
+                                                                 'Mumbai','Mumbai','Mumbai', 'Mumbai','Mumbai','Mumbai',
+                                                                'Mumbai','Mumbai','Mumbai','Mumbai','Mumbai','Mumbai','Mumbai',
+                                                                'Mumbai','Mumbai', 'Mumbai'))
+
+data['joblocation_adress']=data['joblocation_adress'].replace(('Noida','Noida ',' Noida',' Delhi','Delhi','Delhi ',' Delhi ',
+                                                                 'Gurgaon',' Gurgaon',' Gurgaon ','Gurgaon ', ' noida',
+                                                                 ' Noida/Greater Noida',' Noida ', ' Delhi NCR',
+                                                                 'Delhi/NCR(National Capital Region)',' Delhi/NCR ',
+                                                                 ' Delhi/NCR(National Capital Region)',
+                                                                 ' Delhi/NCR(National Capital Region) ',
+                                                                 'Delhi/NCR(National Capital Region) ','Delhi , Delhi',
+                                                                 'Noida , Noida/Greater Noida','Ghaziabad',
+                                                                 'Delhi/NCR(National Capital Region) , Gurgaon',
+                                                                 'NCR , NCR','NCR/NCR(National Capital Region)',
+                                                                'NCR , NCR/Greater NCR','NCR/NCR(National Capital Region), NCR',
+                                                                 'NCR , NCR/NCR(National Capital Region)',
+                                                                 'NCR/NCR(National Capital Region)','NCR/Greater NCR',
+                                                                 'NCR/NCR(National Capital Region) , NCR','Delhi/NCR ',
+                                                                ' Noida/Greater Noida','Greater Noida',' Greater Noida',
+                                                                 ' Greater Noida ','Greater Noida ','Ghaziabad',' Ghaziabad',
+                                                                 'Ghaziabad ',' Ghaziabad ','Faridabad','Faridabad ',
+                                                                 ' Faridabad',' Faridabad ',' Noida/Greater Noida',
+                                                                 ' Noida/Greater Noida ',' delhi',' Delhi/NCR','Delhi NCR'
+                                                                ),
+                                                                ('NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR',
+                                                                'NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR',
+                                                                'NCR','NCR','NCR','NCR','NCR','NCR', 'NCR','NCR','NCR','NCR',
+                                                                 'NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR',
+                                                                 'NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR','NCR',
+                                                                'NCR'))
+
+
+data['joblocation_adress']=data['joblocation_adress'].replace(('Chennai ',' Chennai',' Chennai ',' Chennai',
+                                                                 'chennai ',' chennai',' chennai ',' chennai',),
+                                                                ('Chennai', 'Chennai','Chennai','Chennai','Chennai',
+                                                                'Chennai','Chennai','Chennai',))
+
+data['joblocation_adress']=data['joblocation_adress'].replace(('Pune ',' Pune',' Pune '),('Pune','Pune','Pune'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Kolkata ',' Kolkata',' Kolkata ',
+                                                                ' kolkata','kolkata ',' kolkata'),
+                                                                ('Kolkata','Kolkata','Kolkata',
+                                                                  'Kolkata','Kolkata','Kolkata'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Ahmedabad','Ahmedabad ',' Ahmedabad '),('Ahmedabad',
+                                                                                         'Ahmedabad','Ahmedabad'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Chandigarh ',' Chandigarh',' Chandigarh '),
+                                                                ('Chandigarh','Chandigarh','Chandigarh'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Surat ',' Surat',' Surat '),
+                                                                ('Surat','Surat','Surat'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Ernakulam / Kochi/ Cochin ', ' Kochi', 'Kochi ',' Kochi ',
+                                                                 ' Cochin/ Kochi/ Ernakulam', ' Cochin/ Kochi/ Ernakulam ', 
+                                                                 ' Ernakulam / Kochi/ Cochin',' Ernakulam / Kochi/ Cochin '),
+                                                                ('Kochi','Kochi','Kochi','Kochi','Kochi','Kochi','Kochi',
+                                                                 'Kochi',))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Coimbatore ',' Coimbatore',' Coimbatore '),
+                                                                ('Coimbatore','Coimbatore','Coimbatore'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Coimbatore ',' Coimbatore',' Coimbatore '),
+                                                                ('Coimbatore','Coimbatore','Coimbatore'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Lucknow ',' Lucknow',' Lucknow '),
+                                                                ('Lucknow','Lucknow','Lucknow'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Jaipur ',' Jaipur',' Jaipur ','jaipur ',' jaipur',
+                                                                 ' jaipur '),
+                                                                ('Jaipur','Jaipur','Jaipur','Jaipur','Jaipur','Jaipur'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Vijayawada ',' Vijayawada',' Vijayawada '),
+                                                                ('Vijayawada','Vijayawada','Vijayawada'))
+data['joblocation_adress']=data['joblocation_adress'].replace(('Visakhapatnam ',' Visakhapatnam',' Visakhapatnam ',
+                                                                'Visakhapatnam/Vizag ',' Visakhapatnam/Vizag',
+                                                                 ' Visakhapatnam/Vizag '),
+                                                                ('Visakhapatnam','Visakhapatnam','Visakhapatnam',
+                                                                 'Visakhapatnam','Visakhapatnam','Visakhapatnam',
+                                                                ))
+
+data['joblocation_adress']=data['joblocation_adress'].replace((' Bhubaneshwar',' Bhubaneshwar',' Bhubaneshwar '),
+                                                                ('Bhubaneshwar','Bhubaneshwar','Bhubaneshwar'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Patna',' Patna',' Patna '),
+                                                                ('Patna','Patna','Patna'))
+
+data['joblocation_adress']=data['joblocation_adress'].replace((' Trivandrum',' Trivandrum',' Trivandrum '),
+                                                                ('Trivandrum','Trivandrum','Trivandrum'))
+
+data['joblocation_adress']=data['joblocation_adress'].replace((' Mangalore',' Mangalore',' Mangalore '),
+                                                                ('Mangalore','Mangalore','Mangalore'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Indore',' Indore',' Indore '),
+                                                                ('Indore','Indore','Indore'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Guwahati',' Guwahati',' Guwahati '),
+                                                                ('Guwahati','Guwahati','Guwahati'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Nagpur',' Nagpur',' Nagpur '),
+                                                                ('Nagpur','Nagpur','Nagpur'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Raipur',' Raipur',' Raipur '),
+                                                                ('Raipur','Raipur','Raipur'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Thane',' Thane',' Thane '),
+                                                                ('Thane','Thane','Thane'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Bhopal',' Bhopal',' Bhopal '),
+                                                                ('Bhopal','Bhopal','Bhopal'))
+data['joblocation_adress']=data['joblocation_adress'].replace((' Vadodara/Baroda',' Vadodara/Baroda',' Vadodara/Baroda ',
+                                                                ' Vadodara','Vadodara ',' Vadodara '),
+                                                                ('Vadodara','Vadodara','Vadodara','Vadodara',
+                                                                 'Vadodara','Vadodara',))
+                                                                                                                                  
+
+counts = data['joblocation_adress'].value_counts()
+data = data.loc[data['joblocation_adress'].isin(counts.index[counts > 25])]
+
+pd.set_option('max_rows', 40000)
+data['experience'].str.split(' ')
+
+data['experience'] = data['experience'].str.split(" ")
+data['Min Experience'] = data['experience'].apply(lambda x: x[0])
+data['Max Experience'] = data['experience'].apply(lambda x: x[2] if len(x) > 2 else x[1])
+
+data['Min Experience'].value_counts()
+data['Max Experience'].value_counts()
+
+data['Min Experience'] = data['Min Experience'].replace('Not',0)
+data['Max Experience'] = data['Max Experience'].replace(('Mentioned','-1'), (5,5))
+
+data['Min Experience'] = data['Min Experience'].astype('int')
+data['Max Experience'] = data['Max Experience'].astype('int')
+
+data['education'] = data['education'].fillna('UG: Any Graduate - Any Specialization')
+data['education'].isnull().sum()
+
+data['Education'] = data['education'].str.split(' ')
+data['Education'] = data['Education'].apply(lambda x: x[1] if len(x) > 1 else x[0])
+
+data['Education'].value_counts(dropna = False)
+
+
+data['Education'] = data['Education'].replace(('B.Tech/B.E.','Graduation','Other','-','Not','B.Tech/B.E.,','Postgraduate',
+                                               'PG:CA','Diploma,','B.Com,','B.Pharma,','B.A,','BCA,','B.Sc,','MBA/PGDM','B.B.A,',
+                                              'PG:Other','Doctorate:Doctorate','Post'),
+                                              ('B.Tech','B.Tech','B.Tech','B.Tech','B.Tech','B.Tech','B.Tech',
+                                              'CA','Diploma','B.Com','B.Pharma','B.A','BCA','B.Sc','MBA','BBA',
+                                              'B.Tech','Doctorate','B.Tech'))
+
+data['Education'].value_counts()
+
+counts = data['Education'].value_counts()
+data = data.loc[data['Education'].isin(counts.index[counts >= 25])]
+
+
+data['industry'].str.split(' / ')
+
+data['industry'].isnull().sum()
+
+data['industry'] = data['industry'].fillna(data['industry'].mode()[0])
+
+data['industry'].isnull().sum()
+
+data['Industry'] = data['industry'].str.split(' / ')
+data['Industry'] = data['Industry'].apply(lambda x: x[0])
+
+data['skills'] = data['skills'].fillna(data['skills'].mode()[0])
+data['skills'].isnull().sum()
+
+data['Skills'] = data['skills'].str.split(" - ")
+data['Skills'] = data['Skills'].apply(lambda x: x[1] if len(x) > 1 else x[0])
+
+
+data = data.drop(['education',
+                  'joblocation_address',
+                  'experience',
+                  'industry',
+                  'skills',
+                  'jobid',
+                  'uniq_id',
+                  'site_name'], axis = 1)
+
+data.isnull().sum()
+
+data['numberofpositions'] = data['numberofpositions'].fillna(1)
+data['numberofpositions'] = data['numberofpositions'].astype('int')
+
+data = data.dropna()
+data.isnull().sum().sum()
+
+data['postdate'] = data['postdate'].str.split(" ")
+data['postdate'] = data['postdate'].apply(lambda x: x[0])
+
+data.drop_duplicates(subset = None, keep = 'first', inplace = True)
 
 def show_recommendation_page():
     
     st.subheader("Job Recommenation")
+    x = pd.crosstab(data['Education'],data['Industry'])
 
-    Industry = (
-        
-    )
-    Industry = st.selectbox("Industry", Industry)
+    Industry = st.selectbox("Industry", list(data['Industry'].value_counts().index))
+    job = x[Industry]
+    similar_jobs = x.corrwith(job)
+    similar_jobs = similar_jobs.sort_values(ascending=False)
+    similar_jobs = similar_jobs.iloc[2:]
+    st.write(similar_jobs.head(3))
