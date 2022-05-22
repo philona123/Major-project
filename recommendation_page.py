794f6da8da386488fefca99d02c6f4c1c4b9a1ae
philona123 <philonasebastian44@gmail.com>
2022-05-22 22:18:37 +0530

    deploy 2

--- recommendation_page.py
+++ recommendation_page.py
@@ -2,13 +2,13 @@ import streamlit as st
 import streamlit.components.v1 as stc 
 import matplotlib.pyplot as plt 
 import matplotlib
-matplotlib.use("TkAgg")
+# matplotlib.use("TkAgg")
 import seaborn as sns
 import pandas as pd 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
 import numpy as np
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+# from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 import requests
 from streamlit_lottie import st_lottie
 
